fd447b6478cef058214209fac6f240c017c20669
dennis <devilcool1985@gmail.com>
2022-04-28 08:03:32 +0800

    add search function

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,26 +2,15 @@ import React, { useState } from 'react'
 
 
 
-const Search = ({Find}) => {
-  // const auth = "563492ad6f9170000100000132e9a03b97264e19b38ff3e57245c6c5";
-  // const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
-  // const [data, setData] = useState(null);
-  // const Find = async() =>{
+const Search = ({Find, setInput}) => {
+  const inputHandler = (e)=>{
+    console.log('e.get.value :>> ', e.target.value);
+    setInput(e.target.value)
+  }
 
-  //   const dataFetch = await fetch(initialURL, {
-  //     method: "GET",
-  //     headers: {
-  //       Accept: "application/json",
-  //       Authorization: auth,
-  //     }
-  //   });
-  //   let parseData = await dataFetch.json();
-  //   setData(parseData.photos)
-  //   console.log('data :>> ', data);
-      // }
   return (
     <div className='search' >
-        <input type="text" />
+        <input onChange={inputHandler} type="text" />
         <button onClick={Find}>Search</button>      
     </div>
   )
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,9 +4,12 @@ import Picture from "../components/Picture";
 
 const Homepage = () => {
   const [data, setData] = useState("");
+  const [input, setInput] = useState("");
   const auth = "563492ad6f9170000100000132e9a03b97264e19b38ff3e57245c6c5";
   const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=20";
-  const searchURL = `https://api.pexels.com/v1/search?query=${data}&page=1&per_page=15`;
+  const searchURL = `https://api.pexels.com/v1/search?query=${input}&page=1&per_page=15`;
+  
+  //fetch data from pexel
   const Find = async () => {
     const dataFetch = await fetch(initialURL, {
       method: "GET",
@@ -19,11 +22,27 @@ const Homepage = () => {
     setData(parseData.photos);
     console.log("data :>> ", data);
   };
+
+    //fetch data from pexel
+    const searchData = async () => {
+      const dataFetch = await fetch(searchURL, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          Authorization: auth,
+        },
+      });
+      let parseData = await dataFetch.json();
+      setData(parseData.photos);
+      console.log("data :>> ", data);
+    };
+
+  // show pic when first load
   useEffect(()=>{Find();},[])
   return (
     // <div>123</div>
     <div style={{ minHeight: "90vh" }}>
-      <Search Find={Find} />
+      <Search Find={searchData} setInput={setInput} />
 
       <div className="pictures">
         {data && data.map(d=>{
