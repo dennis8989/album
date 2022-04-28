afc255739a5a6f49399ea5e2ecbd66f19957bf83
dennis <devilcool1985@gmail.com>
2022-04-28 17:31:34 +0800

    0428

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,13 +5,16 @@ import Picture from "../components/Picture";
 const Homepage = () => {
   const [data, setData] = useState("");
   const [input, setInput] = useState("");
+  const [page, setPage] = useState(1);
   const auth = "563492ad6f9170000100000132e9a03b97264e19b38ff3e57245c6c5";
   const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=20";
-  const searchURL = `https://api.pexels.com/v1/search?query=${input}&page=1&per_page=15`;
-  
+
+  const searchURL = `https://api.pexels.com/v1/search?query=${input}&page=${page}&per_page=4`;
+
   //fetch data from pexel
-  const Find = async () => {
-    const dataFetch = await fetch(initialURL, {
+  const Find = async (url) => {
+    setPage(2)
+    const dataFetch = await fetch(url, {
       method: "GET",
       headers: {
         Accept: "application/json",
@@ -20,34 +23,49 @@ const Homepage = () => {
     });
     let parseData = await dataFetch.json();
     setData(parseData.photos);
-    console.log("data :>> ", data);
+    // console.log("data :>> ", data);
   };
 
-    //fetch data from pexel
-    const searchData = async () => {
-      const dataFetch = await fetch(searchURL, {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          Authorization: auth,
-        },
-      });
-      let parseData = await dataFetch.json();
-      setData(parseData.photos);
-      console.log("data :>> ", data);
-    };
+  // loadMore
+  const loadMore = async () => {
+    setPage(page + 1);
+    const dataFetch2 = await fetch(searchURL, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    });
+    let parseData = await dataFetch2.json();
+    parseData.photos.map((d) => setData((data) => [...data, d]));
+    // setData(data.concat(parseData.photos))
+    // console.log("data :>> ", data);
+  };
+  console.log("data :>> ", data);
 
   // show pic when first load
-  useEffect(()=>{Find();},[])
+  useEffect(() => {
+    Find(initialURL);
+    // setPage(2);
+  }, []);
   return (
     // <div>123</div>
     <div style={{ minHeight: "90vh" }}>
-      <Search Find={searchData} setInput={setInput} />
+      <Search
+        Find={() => {
+          Find(searchURL);
+        }}
+        setInput={setInput}
+      />
 
       <div className="pictures">
-        {data && data.map(d=>{
-          return <Picture data={d} />
-        })}
+        {data &&
+          data.map((d) => {
+            return <Picture data={d} />;
+          })}
+      </div>
+      <div className="addmore">
+        <button onClick={loadMore}>Load More</button>
       </div>
     </div>
   );
